6566266b476c131324a9343f6aea02e4f447022b
archmagos <drobertc@yandex.com>
2022-03-22 22:03:12 +0200

    blep

--- server/ShanghAI.py
+++ server/ShanghAI.py
@@ -5,7 +5,7 @@ from bottle import route, run
 ##route it to a page
 @route('/hello')
 def hello():
-    return "Hello World!"
+    return "blep"
 
 #run the server on the host:port combination given
 run(host='localhost', port=8080, debug=True)

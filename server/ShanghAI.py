ea41fd5b9f9e64d35cae524b1973f02f974a7e4c
archmagos-dominus <drobertc@protonmail.com>
2022-03-23 23:44:57 +0200

    load method or something

--- server/ShanghAI.py
+++ server/ShanghAI.py
@@ -1,12 +1,16 @@
 #import bottle to handle connections with HourAI
-from bottle import route, run
+from bottle import get, post, request, run
 
 #define hello function
 ##decorator binds a piece of code to an URL path
 ##in this case, we link the /hello path to the hello() function
-@route('/hello')
-def hello():
-    return "blep"
+@post('/load', method='POST')
+def load():
+    reveived_data = request.json.get('model')
+    print(reveived_data)
+    payload = {"reply": "Loading model..."}
+    reply = dict(data=payload)
+    return reply
 
 #run the server on the host:port combination given
-run(host='localhost', port=8080, debug=True)
+run(host='localhost', port=8000, debug=True)
